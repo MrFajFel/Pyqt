56f3bd8e47b6e98cec577c5e655ea03326ee9809
skiparb <skiparb@gmail.com>
2024-10-07 14:01:06 +0200

    Pierwszy commit

---
+++ main.py
@@ -0,0 +1,20 @@
+import sys
+
+from PyQt6.QtWidgets import QWidget, QApplication
+from PyQt6.QtWidgets.QWidget import window
+
+from layout import Ui_Dialog
+
+
+class Myform(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.ui = Ui_Dialog()
+        self.ui.setupUi(self)
+        self.show()
+
+
+if __name__ == '__main__' :
+    app = QApplication(sys.argv)
+    window = Myform()
+    sys.exit(app.exec())
